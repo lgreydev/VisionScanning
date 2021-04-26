0699702f67e1a9101392776b6dca7134feb94768
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-04-26 16:08:19 +0300

    Initial Commit

---
+++ VisionScanning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VisionScanning
+//
+//  Created by Sergey Lukaschuk on 26.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
